43163ab3d0404594058f4c6d96c8a704a5f509ca
tjtechy <dataskyline.solutions@gmail.com>
2025-03-05 08:59:50 -0500

    update source

--- account_request.tf
+++ account_request.tf
@@ -1,4 +1,5 @@
-resource "aws_controltower_account_factory_request" "new_account" {
+module "ctrlacct {
+  source = "git@github.com:tjtechy11/terraform-control_tower_account_factory.git"
   account_name        = "New-AFT-Account"
   email               = "tjtechy5@gmail.com"
   organizational_unit = "ET"
